7c6d8526224f672d542be2afbc185bac74ef23ad
Fredrik <fredrik.midtfla@gmail.com>
2026-02-03 13:36:58 +0100

    Added calculator operations (+, -, *, /)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    static double stringToDouble(string str)
+    {
+        double n = -1;
+        bool status = double.TryParse(str, out n);
+        if (status == false)
+        {
+            throw new Exception("Failed to parse");
+        }
+        return n;
+    }
+
+    static void Main(string[] args)
+    {
+        Calculator calculator = new Calculator();
+
+        
+    }
+}
---
+++ classes/Calculator.cs
@@ -0,0 +1,30 @@
+
+
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualBasic;
+
+public class Calculator
+{
+    public double addNumbers(double a, double b){
+        return a + b;
+    }
+
+    public double subtractNumbers(double a, double b)
+    {
+        return a - b;
+    }
+
+    public double multiplyNumbers(double a, double b)
+    {
+        return a * b;
+    }
+
+    public double divideNumbers(double a, double b)
+    {
+        return a / b;
+    }
+    public void operation()
+    {
+        
+    }
+}
